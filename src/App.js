de8a2b7e6a92862731351b7706bbac8066f27793
Richard Awale <richardawale@Richards-MacBook-Pro.local>
2020-06-10 15:57:14 +0300

    first react commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props){
+  return(
+    <div className="person"> 
+      <h1> Name: {props.name} </h1>
+      <p> Your age :{props.age} </p>
     </div>
+    
   );
 }
+
+var apple=(
+  <div>
+    <App name="Richard" age="25" />
+    <App name="awale" age="25" />
+  </div>
+);
+
+ReactDOM.render(apple, document.querySelector("#app"));
 
 export default App;
